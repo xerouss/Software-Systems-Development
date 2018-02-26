e2a121eee8e1d454d343f78596867c5033f3286d
xerouss <1wongs50@solent.ac.uk>
2018-02-26 19:41:28 +0000

    Created main menu and hardware list
    Start form changed to login. Main and hardware list forms created. Main menu is set up with a title and buttons. Only the hardware list button works at the moment. The hardware list displays some already inputted data. Has a back button to go back to the main menu.

---
+++ HardwareList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game_Café_Demonstration_Program
+{
+    public partial class HardwareList : Form
+    {
+        public HardwareList()
+        {
+            InitializeComponent();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void ReturnToMenuButton_Click(object sender, EventArgs e)
+        {
+            // Create a hardware form to show while closing this form
+            MainMenu mainMenu = new MainMenu();
+            this.Hide();
+            mainMenu.ShowDialog();
+            this.Close();
+        }
+    }
+}
