8c28cd80800147e1797d53fed503e56cbae52b3a
xerouss <1wongs50@solent.ac.uk>
2018-03-06 16:21:01 +0000

    Continued trying to get the database working
    There are notes in the hardware registration code that needs o be carried out.

--- Forms/Add to list/HardwareRegistration.cs
+++ Forms/Add to list/HardwareRegistration.cs
@@ -31,24 +31,30 @@ namespace Game_Café_Demonstration_Program
 
         private void SubmitButton_Click(object sender, EventArgs e)
         {
-            string hardware = HardwareDropDown.SelectedText;
-            string peripheral = PeripheralDropDown.SelectedText;
+            // https://stackoverflow.com/questions/10263094/executenonquery-connection-property-has-not-been-initialized
+            // http://www.c-sharpcorner.com/UploadFile/009464/insert-data-into-database-in-window-form-using-C-Sharp/
 
-            SqlConnection con = new SqlConnection(@"Server=localhost\SQLExpress;"
-       + "Database=HardwareDatabase;");
+            // NEED TO CREATE A PROCEDURE FROM THIS FOR IT TO WORK
+            // CANNOT DO IT AT UNI BECAUSE I THINK ITS BLOCKING ACCESS
+            // NEED TO CHANGE THE HARDWARE AND PERIPHERAL VALUES TO VARCHAR
+            // AND CHANGE HARDWARE FIELD TO HARDWARE TYPE
+            // USE ONE DATABASE AS WELL INSTEAD OF MULTIPLE FOR EVERYTHING AND JUST DO NEW TABLES
+            //https://stackoverflow.com/questions/42868370/stored-procedure-not-found
 
-            // SqlConnection con = new SqlConnection("Data Source=.\SQLEXPRESS;" +
-            //    "AttachDbFilename=|DataDirectory|\Databases\HardwareDatabase.mdf;Integrated Security=True;User Instance=True");
+            string hardware = HardwareDropDown.SelectedItem.ToString();
+            string peripheral = PeripheralDropDown.SelectedItem.ToString();
 
-            //SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;" +
-            //        "User Instance=true;" +
-            //        "Integrated Security=true;" +
-            //        "AttachDbFilename=|DataDirectory|HardwareDatabase.mdf;");
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Databases\HardwareRegistrationDatabase.mdf;Integrated Security=True;");
 
+            // SqlCommand cmd = new SqlCommand("INSERT INTO [dbo].[Hardware] ([Id], [Hardware], [Peripheral])" +
+            //    "VALUES (@ID, @Hardware, @Peripheral);");
             SqlCommand cmd = new SqlCommand("sp_insert", con);
+
+            //cmd.Connection = con;
             cmd.CommandType = CommandType.StoredProcedure;
-            cmd.Parameters.AddWithValue("@Hardware", hardware);
-            cmd.Parameters.AddWithValue("@Peripheral", peripheral);
+            cmd.Parameters.AddWithValue("@ID", SqlDbType.Int).Value = 0;
+            cmd.Parameters.AddWithValue("@Hardware", SqlDbType.VarChar).Value = hardware;
+            cmd.Parameters.AddWithValue("@Peripheral", SqlDbType.VarChar).Value = peripheral;
 
             con.Open();
             int i = cmd.ExecuteNonQuery();
